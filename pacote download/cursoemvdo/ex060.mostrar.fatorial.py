79f0d6b8ef10e42c9d1f63227e1ec16a0b4bc3d4
fernandaveiga <70073518+fernandaveiga@users.noreply.github.com>
2020-09-26 08:16:13 -0300

    rename
    rename e exclusão da segunda resolução

--- pacote download/cursoemvdo/ex060.mostrar.fatorial.py
+++ pacote download/cursoemvdo/ex060.mostrar.fatorial.py
@@ -7,13 +7,3 @@ for c in range(n,0,-1):
     print('{}'.format(c), end=' ')
     print('x' if c>1 else '=', end=' ')
 print(f)
-
-'''n=int(input('Digite um numero: ')
-c=n
-f=1
-while c>0:
-    print('{}'.format(c), end='')
-    print(' x ' if c>1 else '=', end=''
-    f=f*c
-    c=c-1
-print('{}'.format (f))'''
